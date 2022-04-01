0024c5cfaac9dc1cb092a019c643f2b80e08509b
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-01 03:25:22 +0000

    fixed errors and added code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Created by: Emmanuel.Fofeyin-Ngwamunwi
+// Created on: march 2022
+// This file contains the JS functions for index.html
+
+/**
+ * do basic math
+ */
+function doMathClicked() {
+
+  document.getElementById('add-math').innerHTML = '<p>440 - 440' + (3 + 0) + '</p>'
+  document.getElementById('add-math').innerHTML = '<p>440 - 440' + (5 + 0) + '</p>'
+  document.getElementById('subtract-math').innerHTML = '<p>2 + 5 × 1 = ' + (8 - 2) + '</p>'
+document.getElementById('subtract-math').innerHTML = '<p>3 + 6 × 1 = ' + (8 - 0) + '</p>'
+}      
--- main.cs
+++ main.cs
@@ -1,7 +1,38 @@
+// Created by: Emmanuel Fofeyin
+// Created on: March 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        string shapeRectangle;
+        int area;
+        int perimeter;
+
+        Console.WriteLine("This program finds the area and perimeter of a rectangle.");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter length (mm): 3");
+        Console.WriteLine("Enter width (mm): 5");
+        
+        Console.WriteLine("The area is: 15 mm²");
+        Console.WriteLine("The perimeter is: 16 mm");
+        shapeRectangle = Console.ReadLine();
+        Console.Write("Enter your area: ");
+        Console.Write("Enter your perimeter: ");
+        area = Convert.ToInt32(Console.ReadLine());
+        perimeter = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + shapeRectangle + ", area " + area + ".");
+         Console.WriteLine("");
+        Console.WriteLine("Your info is: " + shapeRectangle + ", perimeter " + perimeter + ".");
+
+        Console.WriteLine("\nDone.");
+    }
 }
